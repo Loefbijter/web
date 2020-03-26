69005f24cb3c261a96de612df9fa6bf740f73f99
bahfheuv <38426620+bahfheuv@users.noreply.github.com>
2020-03-26 17:17:56 +0100

    TSLint (#1)
    * Done
    
    * Added angular/recommended rules
    
    * Update app.component.ts
    
    * Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
+/*tslint:disable:no-floating-promises*/
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,25 +13,27 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents()
+    // tslint:disable-next-line: no-console
+    .catch((e: object) => console.error(`Could not setup TestBed in ${__filename}:\n${e}`));
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'loefbijter-web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it("should have as title 'loefbijter-web'", () => {
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.componentInstance;
     expect(app.title).toEqual('loefbijter-web');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
+    const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span').textContent).toContain('loefbijter-web app is running!');
   });
 });
