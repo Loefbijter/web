56e5c170b67eb2bbc444854624440088c6223479
dutchmartin <martijn.groeneveldt@gmail.com>
2020-07-24 16:25:00 +0200

    make it hopefully work

--- .github/workflows/set_environment.sh
+++ .github/workflows/set_environment.sh
@@ -4,10 +4,9 @@
 # Copies the example environment file and sets the correct values through a string find and replace.
 
 echo "Executing script from: $PWD"
-ls -a
 
 cp $PWD/src/environments/environment.example.ts $PWD/src/environments/environment.prod.ts
 sed -i 's/production\: false/production\: true/' $PWD/src/environments/environment.prod.ts
 SEDCMD="s|http\:\/\/api\.com\/api|$1|"
 sed -i $SEDCMD $PWD/src/environments/environment.prod.ts
-ls $PWD/src/environments/ -a
+cp $PWD/src/environments/environment.prod.ts $PWD/src/environments/environment.ts
