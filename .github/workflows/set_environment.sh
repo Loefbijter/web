f89ecccd12a8d64a5f9921af9db14cacd66213d1
dutchmartin <martijn.groeneveldt@gmail.com>
2020-07-28 21:26:19 +0200

    set correct vars for production

--- .github/workflows/set_environment.sh
+++ .github/workflows/set_environment.sh
@@ -4,9 +4,4 @@
 # Copies the example environment file and sets the correct values through a string find and replace.
 
 echo "Executing script from: $PWD"
-
-cp $PWD/src/environments/environment.example.ts $PWD/src/environments/environment.prod.ts
-sed -i 's/production\: false/production\: true/' $PWD/src/environments/environment.prod.ts
-SEDCMD="s|http\:\/\/api\.com\/api|$1|"
-sed -i $SEDCMD $PWD/src/environments/environment.prod.ts
 cp $PWD/src/environments/environment.prod.ts $PWD/src/environments/environment.ts
