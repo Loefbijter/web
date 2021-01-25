fadf759877630b45510ac4648409385e988f7258
dutchmartin <martijn.groeneveldt@gmail.com>
2021-01-25 16:57:49 +0100

    add extra page for users to not get lost after a password reset.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { SetPasswordComponent } from './set-password/set-password.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { RoleGuard } from './_helpers/role.guard';
 import { UserRole } from './_helpers/auth.model';
+import {PasswordSetComponent} from "./password-set/password-set.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'set-password/:token', component: SetPasswordComponent },
+  { path: 'password-set', component: PasswordSetComponent },
   {
     path: 'boats',
     loadChildren: () => import('./boats/boats.module').then(m => m.BoatsModule),
--- src/app/password-set/password-set.component.ts
+++ src/app/password-set/password-set.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentItem } from "../_modules/content/content-item.model";
+import { ContentService } from "../_modules/content/content.service";
+import {Router} from "@angular/router";
+
+// tslint:disable-next-line: no-var-requires
+const content: ContentItem = require('./password-set.content.json');
 
 @Component({
   selector: 'app-password-set',
@@ -7,9 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordSetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly contentService: ContentService,
+    private readonly router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.contentService.addContentItems(content);
   }
 
+  async goHome(){
+    await this.router.navigateByUrl("/");
+  }
 }
